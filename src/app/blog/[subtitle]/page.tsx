401a9503e579b97ae608127413d19a4592952be9
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-09-13 19:25:49 +0545

    generateStaticParams

--- src/app/blog/[subtitle]/page.tsx
+++ src/app/blog/[subtitle]/page.tsx
@@ -8,4 +8,13 @@ const BlogDetailsPage = ({}: BlogDetailsPageProps) => {
   return <DoubleSidebar>Hello</DoubleSidebar>;
 };
 
+export async function generateStaticParams() {
+  const posts = await fetch("https://.../posts").then((res) => res.json());
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return posts.map((post: any) => ({
+    slug: post.slug,
+  }));
+}
+
 export default BlogDetailsPage;
