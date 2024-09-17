c8d2ba50c306bdd704e66b924fdc11f2121cd6f9
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-09-17 10:41:36 +0545

    generate static params

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,48 +1,14 @@
-"use client";
-
 import { getAllPosts, getPostBySlug } from "@/actions/blog.action";
 import ContentSection from "@/components/shared/blog/content";
 import DoubleSidebar from "@/components/shared/doublesidebar";
 import { Post } from "@/types";
-import { useEffect, useState } from "react";
 
 interface BlogDetailsPageProps {
   params: { slug: string };
 }
 
-const BlogDetailsPage = ({ params }: BlogDetailsPageProps) => {
-  const [post, setPost] = useState<Post>({
-    date: new Date(),
-    slug: "",
-    preview: "",
-    title: "",
-    tags: "",
-    timeToRead: "",
-    topics: "",
-    image: "",
-    content: "",
-    category: "",
-    createdAt: new Date(),
-    id: "",
-  });
-  useEffect(() => {
-    const fetchPost = async () => {
-      const response = await getPostBySlug(params.slug, [
-        "date",
-        "slug",
-        "preview",
-        "title",
-        "tags",
-        "timeToRead",
-        "topics",
-        "preview",
-        "image",
-        "content",
-      ]);
-      setPost(response);
-    };
-    fetchPost();
-  }, [params.slug]);
+const BlogDetailsPage = async ({ params }: BlogDetailsPageProps) => {
+  const post = (await getPostBySlug(params.slug, ["content"])) as Post;
 
   return (
     <DoubleSidebar selectedPost={post}>
