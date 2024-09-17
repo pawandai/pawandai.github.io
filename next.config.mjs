93178d46dcc24cd10eff4e9d0168c4d68d053863
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-09-17 11:47:50 +0545

    generate static params

--- next.config.mjs
+++ next.config.mjs
@@ -8,13 +8,9 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: false,
-      };
-    }
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
 
     return config;
   },
